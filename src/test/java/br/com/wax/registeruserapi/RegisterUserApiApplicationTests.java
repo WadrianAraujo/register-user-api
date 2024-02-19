db21b1355c87ed9feb415e708472d23142638820
Wadrian Araujo <83624647+WadrianAraujo@users.noreply.github.com>
2024-02-19 14:47:37 -0400

    add project base

---
+++ src/test/java/br/com/wax/registeruserapi/RegisterUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wax.registeruserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
