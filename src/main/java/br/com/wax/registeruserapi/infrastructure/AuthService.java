40316e2658a2f7178601d6b2ef829142e58cd8bc
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2024-02-20 14:18:55 -0400

    add SecurityConfig

---
+++ src/main/java/br/com/wax/registeruserapi/infrastructure/AuthService.java
@@ -0,0 +1,20 @@
+package br.com.wax.registeruserapi.infrastructure;
+
+import br.com.wax.registeruserapi.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
