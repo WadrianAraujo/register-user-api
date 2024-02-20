1e3c6276f7db2d3fd80cbeb491e5be3dfeb5f26d
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2024-02-20 14:19:23 -0400

    add user entity

--- src/main/java/br/com/wax/registeruserapi/domain/entity/User.java
+++ src/main/java/br/com/wax/registeruserapi/domain/entity/User.java
@@ -4,27 +4,30 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
 @Table(name = "users")
 @Entity(name = "User")
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 5, max =  100, message = "The name need from 5 to 100 characters")
-    @NotBlank(message = "The name is necessary")
-    private String name;
+    @Size(min = 5, max =  100, message = "The username need from 5 to 100 characters")
+    @NotBlank(message = "The username is necessary")
+    private String login;
 
     @Email
     private String email;
 
-    @Size(min = 8, max = 25, message = "The password need from 5 to 25 characters")
-    @NotBlank(message = "the password is necessary")
-    private String password;
+    @Size(max = 15, message = "The cell phone number cannot exceed 15 characters")
+    private String phoneNumber;
+
+
+
 }
