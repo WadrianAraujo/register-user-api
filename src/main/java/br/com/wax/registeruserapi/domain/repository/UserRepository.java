2c2ff266df76b937d42bf460b181cf756426bac1
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2024-02-20 14:19:52 -0400

    add user repository

--- src/main/java/br/com/wax/registeruserapi/domain/repository/UserRepository.java
+++ src/main/java/br/com/wax/registeruserapi/domain/repository/UserRepository.java
@@ -2,11 +2,11 @@ package br.com.wax.registeruserapi.domain.repository;
 
 import br.com.wax.registeruserapi.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
-
+    UserDetails findByLogin(String login);
 
 }
